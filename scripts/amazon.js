b7cd77c9603756b44fed2b622d6892d8005444db
Piyush-Sharma79 <g2001idseptember@gmail.com>
2023-12-18 19:50:43 +0530

    made the select list working

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -22,7 +22,7 @@ product.forEach((product) => {
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class= "js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -55,6 +55,8 @@ document.querySelectorAll('.js-add-to-cart')
 .forEach(button => {
     button.addEventListener('click', ()=>{
         const productId = button.dataset.productId;
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+        const quantity = Number(quantitySelector.value);
         let matchingItem;
         cart.forEach(item => {
             if(productId === item.productId){
@@ -62,13 +64,13 @@ document.querySelectorAll('.js-add-to-cart')
             }
         });
         if(matchingItem){
-            matchingItem.quantity += 1;
+            matchingItem.quantity += quantity;
         }
         else{
             cart.push({
                 productId : productId,
-                quantity :1
-            })
+                quantity :quantity
+            });
         }
         let cartQuantity = 0;
         cart.forEach(item => {
