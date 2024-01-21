cfdaff173786f5987093ac26e32d818bdfba6f92
Piyush-Sharma79 <g2001idseptember@gmail.com>
2024-01-21 20:32:49 +0530

    exercises

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -9,11 +9,14 @@ describe('test suite:formatcurrency',()=>{
         expect(formatCurrency(0)).toEqual('0.00');
     });
     describe('rounding',()=>{
-        it('rounds upto nearest cent',()=>{
+        it('rounds down nearest cent',()=>{
             expect(formatCurrency(2000.4)).toEqual('20.00');
         });
         it('rounds upto nearest cent',()=>{
             expect(formatCurrency(2000.5)).toEqual('20.01');
-        })
+        });
+        it('works with negative numbers', () => {
+            expect(formatCurrency(-500)).toEqual('-5.00');
+        });
     })
 });
